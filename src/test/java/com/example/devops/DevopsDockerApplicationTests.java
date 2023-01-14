28511825ba97860478ecfdc31dbcc5cb76a62c2a
ahmed <ahmedyoussofafit@gmail.com>
2023-01-14 11:34:21 +0200

    Create Dockerfile

--- src/test/java/com/example/devops/DevopsDockerApplicationTests.java
+++ src/test/java/com/example/devops/DevopsDockerApplicationTests.java
@@ -6,14 +6,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DevopsDockerApplicationTests {
 	
 	@Autowired
-	  MockMvc mockMvc;
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
