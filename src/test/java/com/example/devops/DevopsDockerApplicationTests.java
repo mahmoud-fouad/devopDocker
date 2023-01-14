336a799bbb9a61cff8915604fb55f114001720e3
Mahmoud Fuad <mahmoud.ah.fouad@gmail.com>
2023-01-14 10:47:08 +0200

    add test cases to check that api is running

--- src/test/java/com/example/devops/DevopsDockerApplicationTests.java
+++ src/test/java/com/example/devops/DevopsDockerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.devops;
 
+import static org.assertj.core.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class DevopsDockerApplicationTests {
+	
+	@Autowired
+	  MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		
+		 try {
+			mockMvc.perform(get("/api"))
+			    .andExpect(status().isOk());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("system not running");
+		}
 	}
 
 }
